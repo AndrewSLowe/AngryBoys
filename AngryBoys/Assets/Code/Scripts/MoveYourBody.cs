6eb7ee5189ede8285b016525eae32790870c3b3d
Andrew Lowe <andrewstevenlowe@gmail.com>
2024-01-12 22:52:17 -0600

    AL-0001 Added running and idle animations for player :D

--- AngryBoys/Assets/Code/Scripts/MoveYourBody.cs
+++ AngryBoys/Assets/Code/Scripts/MoveYourBody.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,25 +7,32 @@ public class PlayerController : MonoBehaviour
     private BoxCollider2D coll;
     private Rigidbody2D rb;
     [SerializeField] private LayerMask jumpableGround;
-    private float moveSpeed = 5f;
-    private float jumpForce = 3f;
+    [SerializeField] public float moveSpeed = 5f;
+    [SerializeField] public float jumpForce = 3f;
+    private Animator anim;
+    private float dirX = 0f;
     
     private void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
 
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal"); // Raw lets you get back to 0 immediately
+        dirX = Input.GetAxisRaw("Horizontal"); // Raw lets you get back to 0 immediately
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+       
+        UpdateAnimation();
+        
     }
 
     private bool IsGrounded()
@@ -32,4 +40,19 @@ public class PlayerController : MonoBehaviour
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 
+    private void UpdateAnimation()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
 }
