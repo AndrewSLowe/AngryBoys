82759c4d95752165df18aa9b2538143b60af0e5e
Andrew Lowe <andrewstevenlowe@gmail.com>
2024-01-13 22:53:32 -0600

    AL-0001 Added checkpoint at end of level that takes player to next level.

--- AngryBoys/Assets/Code/Scripts/Finish.cs
+++ AngryBoys/Assets/Code/Scripts/Finish.cs
@@ -2,26 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
+    private Animator anim;
+    private bool levelCompleted = false;
+    [SerializeField] private AudioSource bgMusic;
+    
     private void Start()
     {
         finishSound = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
+            DisablePlayerMovement(collision);
+            AnimatePlayerFinish(collision);
+            bgMusic.Stop();
             finishSound.Play();
-            CompleteLevel();
+            // anim.SetTrigger("finish");
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void DisablePlayerMovement(Collider2D collision)
+    {
+        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
 
+        if (playerController != null)
+        {
+            playerController.enabled = false;
+        }
+    
+    }
+    private void AnimatePlayerFinish(Collider2D collision)
+    {
+        Animator playerAnimator = collision.gameObject.GetComponent<Animator>();
+        if (playerAnimator != null)
+        {
+            playerAnimator.SetTrigger("finish"); // Replace 'finish' with the name of your trigger
+        }
     }
 }
